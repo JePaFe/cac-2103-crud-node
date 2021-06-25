303674447ea487e9c5e601ff522353c7db889c4e
Jean Paul Ferreira <jepafe@hotmail.com>
2021-06-25 09:07:17 -0300

    View Engine EJS

--- router.js
+++ router.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const productos = [
+    {id: 1, name: 'Producto Nro 1'},
+    {id: 2, name: 'Producto Nro 2'},
+    {id: 3, name: 'Producto Nro 3'},
+    {id: 4, name: 'Producto Nro 4'},
+]
+
 router.get('/', (req, res) => {
-    res.send('Hola desde express')
+    res.render('index')
 })
 
 router.get('/productos', (req, res) => {
-    res.send('Listado de productos')
+    res.render('productos/index', { productos })
+})
+
+router.get('/productos/:id', (req, res) => {
+    const producto = productos.find(producto => producto.id == req.params.id)
+    res.render('productos/show',  { producto })
 })
 
 module.exports = router
