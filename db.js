a53cef9ab0ca2bb9b15b06d7bf49e081f46c4ccd
Jean Paul Ferreira <jeanpaul@jepage.net>
2021-07-17 15:18:39 -0300

    Config DB

--- db.js
+++ db.js
@@ -3,11 +3,10 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'node'
+    database: process.env.DB_NAME
 })
 
 connection.connect((err) => {
